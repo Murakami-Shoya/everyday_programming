504b97f891bf5dbca1d2ecec18b4087ce6b068b6
MurakamiShoya <murashoya@gmail.com>
2021-09-27 23:31:34 +0900

    20210927

--- everyday_python/20210922.py
+++ everyday_python/20210922.py
@@ -1,1 +1,60 @@
-print "hello"
+import Leap, sys, thread, time
+from Leap import CircleGesture, KeyTapGesture, ScreenTapGesture, SwipeGesture
+
+class LeapListener(Leap.Listener):
+    finger_names = ['Thumb', 'Index', 'Middle', 'Ring', 'Pinky']
+    bone_names = ['Metacarpal', 'Proximal', 'Intermediate', 'Distal']
+    state_names = ['STATE_INVALID', 'STATE_START', 'STATE_UPDATE', 'STATE_END']
+
+    def on_init(self, controller):
+        print "Initialized"
+
+    def on_connect(self, controller):
+        print "Connected"
+
+        # Enable gestures
+        controller.enable_gesture(Leap.Gesture.TYPE_CIRCLE);
+        controller.enable_gesture(Leap.Gesture.TYPE_KEY_TAP);
+        controller.enable_gesture(Leap.Gesture.TYPE_SCREEN_TAP);
+        controller.enable_gesture(Leap.Gesture.TYPE_SWIPE);
+
+    def on_disconnect(self, controller):
+        # Note: not dispatched when running in a debugger.
+        print "Disconnected"
+
+    def on_exit(self, controller):
+        print "Exited"
+
+    def on_frame(self, controller):
+        frame = controller.frame()
+        
+        # for hand in frame.hands:
+        for hand in frame.hands:
+            for finger in hand.fingers:
+                print "%s finger, id: %d, length: %fmm, width: %fmm" % (
+                    self.finger_names[finger.type],
+                    finger.id,
+                    finger.length,
+                    finger.width)
+
+
+def main():
+    # Create a sample listener and controller
+    listener = LeapListener()
+    controller = Leap.Controller()
+
+    # Have the sample listener receive events from the controller
+    controller.add_listener(listener)
+
+    # Keep this process running until Enter is pressed
+    print "Press Enter to quit..."
+    try:
+        sys.stdin.readline()
+    except KeyboardInterrupt:
+        pass
+    finally:
+        # Remove the sample listener when done
+        controller.remove_listener(listener)
+
+if __name__ == "__main__":
+    main()
